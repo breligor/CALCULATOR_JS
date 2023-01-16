c25abb354a835008601a4e4ba70334355523ffa2
breligor <77774848+breligor@users.noreply.github.com>
2023-01-16 13:23:06 +0300

    feat: add function to switch color theme

---
+++ src/scripts/themeSwitch.js
@@ -0,0 +1,24 @@
+const themeSwitchBtn = document.querySelector(".themeSwitch-btn");
+const link = document.querySelector('[title="theme"]');
+
+
+
+function applyTheme(theme) {
+  const lightTheme = "/src/style/css/light.css";
+  const darkTheme = "/src/style/css/dark.css";
+  let currentTheme = link.getAttribute("href");
+  theme = "";
+
+  if (currentTheme === lightTheme) {
+    currentTheme = darkTheme;
+    theme = "dark";
+  } else {
+    currentTheme = lightTheme;
+    theme = "light";
+  }
+  link.setAttribute("href", currentTheme);
+}
+
+themeSwitchBtn.addEventListener("click", () => {
+  applyTheme();
+});
